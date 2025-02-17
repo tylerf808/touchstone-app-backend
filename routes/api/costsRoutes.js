105691cdfaaac048d249852356447fd8795b510b
Tyler Fabian <tylerf808@gmail.com>
2025-02-17 17:00:13 -0500

    Changes made sending route back

--- routes/api/costsRoutes.js
+++ routes/api/costsRoutes.js
@@ -123,7 +123,7 @@ router.post('/check', auth, async (req, res) => {
       tractor: tractor.internalNum
     }
 
-    res.status(200).json(newJob)
+    res.status(200).json([newJob, routeResponse])
 
   } catch (error) {
     console.log(error)
