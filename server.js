e0a16f44272eaa118723b4295a76ee5eca0a3149
Tyler Fabian <tylerf808@gmail.com>
2025-02-24 15:46:45 -0500

    New server file fix

--- server.js
+++ server.js
@@ -1,27 +1,31 @@
-const express = require('express')
-const routes = require('./routes')
-const db = require('./config/connection')
-const cors = require('cors')
-const path = require('path')
-require('dotenv').config()
+const express = require("express");
+const routes = require("./routes");
+const db = require("./config/connection");
+const cors = require("cors");
+const path = require("path");
+require("dotenv").config();
 
-const PORT = process.env.PORT || 8080
-const app = express()
+const PORT = process.env.PORT || 8080;
+const app = express();
 
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
-app.use('/', express.static(__dirname + 'public'))
+// Serve static files from React build
+app.use(express.static(path.join(__dirname, "client", "build")));
 
+// API routes
+app.use(routes);
+
+// Serve React app for all non-API requests
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
+// Start server
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}`);
   });
-
-app.use(routes)
-
-db.once('open', () => {
-    app.listen(PORT, () => {
-        console.log(`API server running on port ${PORT}`)
-    })
-})
+});
