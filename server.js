89d1e99af79fc42bdb4913dfa6466e742455e15b
Tyler Fabian <tylerf808@gmail.com>
2025-02-25 15:46:54 -0500

    Added reroute

--- server.js
+++ server.js
@@ -1,31 +1,21 @@
-const express = require("express");
-const routes = require("./routes");
-const db = require("./config/connection");
-const cors = require("cors");
-const path = require("path");
-require("dotenv").config();
+const express = require('express')
+const routes = require('./routes')
+const db = require('./config/connection')
+const cors = require('cors')
+require('dotenv').config()
 
-const PORT = process.env.PORT || 8080;
-const app = express();
+const PORT = process.env.PORT || 8080
+const app = express()
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json());
-app.use(cors());
+app.use(cors())
 
-// Serve static files from React build
-app.use(express.static(path.join(__dirname, "client", "build")));
+app.use('/', express.static(__dirname + 'public'))
+app.use(routes)
 
-// API routes
-app.use(routes);
-
-// Serve React app for all non-API requests
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
-
-// Start server
-db.once("open", () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}`);
-  });
-});
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`API server running on port ${PORT}`)
+    })
+})
