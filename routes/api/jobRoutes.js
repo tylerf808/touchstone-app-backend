8f5bfcd506e927bd4977a643861d98f5ac0860ec
Tyler Fabian <tylerf808@gmail.com>
2025-09-22 15:41:22 -0400

    Added jobs endpoint for deleting an array of jobs

--- routes/api/jobRoutes.js
+++ routes/api/jobRoutes.js
@@ -10,7 +10,6 @@ router.post('/allJobs', auth, async (req, res) => {
         const jobData = await Job.find({ admin: req.user.username })
         res.status(200).json(jobData);
     } catch (error) {
-        console.log(error)
         res.status(500).json(error)
     }
 })
@@ -22,7 +21,19 @@ router.post('/newJob', auth, async (req, res) => {
         const newJob = await Job.create(req.body)
         res.status(200).json(newJob)
     } catch (error) {
-        console.log(error)
+        res.status(500).json(error)
+    }
+})
+
+//Delete batch of jobs
+router.post('/deleteJobs', auth, async (req, res) => {
+    try {
+        const selectedJobs = req.body.jobs
+        selectedJobs.forEach(async (job) => {
+            await Job.findOneAndDelete({_id: job._id})
+        })
+        res.status(200)
+    } catch (error) {
         res.status(500).json(error)
     }
 })
