50033a2870794b4f80f564b89a1afb94a46b7ed2
Tyler Fabian <tylerf808@gmail.com>
2025-10-27 14:58:05 -0400

    Added console log for newJob error to view in the logs on render

--- routes/api/jobRoutes.js
+++ routes/api/jobRoutes.js
@@ -21,6 +21,7 @@ router.post('/newJob', auth, async (req, res) => {
         const newJob = await Job.create(req.body)
         res.status(200).json(newJob)
     } catch (error) {
+        console.log(error)
         res.status(500).json(error)
     }
 })
